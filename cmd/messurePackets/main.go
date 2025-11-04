e1d288125092c8011b63d0f319ec0f477c8f037a
Gustav Bruhn <gustav.j.bruhn@accenture.com>
2025-11-04 13:49:39 +0100

    add bandwidth calculation and all types of sketches

--- cmd/messurePackets/main.go
+++ cmd/messurePackets/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -14,6 +15,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/proto"
 )
 
 var SERVER_ADR = "127.0.0.1"
@@ -25,6 +27,10 @@ var NUM_MERGES = 10000
 var DATA_SET_PATH = "../../data/PVS 1/dataset_gps.csv"
 var HEADER_NAME = "speed_meters_per_second"
 var meregesMade uint64 = 0
+var mesuringInterval = 5.0
+var timeUnit = time.Nanosecond
+var timeExponent = 1 / (float64(timeUnit) / float64(time.Second))
+var sketchType = "kll"
 
 func startRealConnection(adr string) (pb.SketcherClient, *grpc.ClientConn, error) {
 
@@ -64,9 +70,78 @@ func startKll[T shared.Number](wg *sync.WaitGroup, fg *sync.WaitGroup, cond *syn
 
 	for range merges {
 
-		time.Sleep(time.Duration(streamRate*mergeRate) * time.Nanosecond)
+		time.Sleep(time.Duration(streamRate*mergeRate) * timeUnit)
 		go func() {
 			client.MakeRequest[pb.KLLSketch](sketch, SERVER_ADR, c.MergeKll, conn, &c, startRealConnection, reconAttempt)
+			atomic.AddUint64(&meregesMade, 1)
+		}()
+	}
+	return
+}
+func startCount[T shared.Number](wg *sync.WaitGroup, fg *sync.WaitGroup, cond *sync.Cond, streamRate int, mergeRate int, sketch *pb.CountSketch, merges int) {
+	c, conn, err := startRealConnection(SERVER_ADR + ":" + PORT)
+
+	var reconAttempt *int = new(int)
+	if err != nil {
+		panic("test failed because no connection")
+	}
+	wg.Done()
+	cond.L.Lock()
+
+	cond.Wait()
+	cond.L.Unlock()
+
+	for range merges {
+
+		time.Sleep(time.Duration(streamRate*mergeRate) * timeUnit)
+		go func() {
+			client.MakeRequest[pb.CountSketch](sketch, SERVER_ADR, c.MergeCount, conn, &c, startRealConnection, reconAttempt)
+			atomic.AddUint64(&meregesMade, 1)
+		}()
+	}
+	return
+}
+func startBadKll[T shared.Number](wg *sync.WaitGroup, fg *sync.WaitGroup, cond *sync.Cond, streamRate int, mergeRate int, sketch *pb.BadArray, merges int) {
+	c, conn, err := startRealConnection(SERVER_ADR + ":" + PORT)
+
+	var reconAttempt *int = new(int)
+	if err != nil {
+		panic("test failed because no connection")
+	}
+	wg.Done()
+	cond.L.Lock()
+
+	cond.Wait()
+	cond.L.Unlock()
+
+	for range merges {
+
+		time.Sleep(time.Duration(streamRate*mergeRate) * timeUnit)
+		go func() {
+			client.MakeRequest[pb.BadArray](sketch, SERVER_ADR, c.BadKll, conn, &c, startRealConnection, reconAttempt)
+			atomic.AddUint64(&meregesMade, 1)
+		}()
+	}
+	return
+}
+func startBadCount[T shared.Number](wg *sync.WaitGroup, fg *sync.WaitGroup, cond *sync.Cond, streamRate int, mergeRate int, sketch *pb.BadArray, merges int) {
+	c, conn, err := startRealConnection(SERVER_ADR + ":" + PORT)
+
+	var reconAttempt *int = new(int)
+	if err != nil {
+		panic("test failed because no connection")
+	}
+	wg.Done()
+	cond.L.Lock()
+
+	cond.Wait()
+	cond.L.Unlock()
+
+	for range merges {
+
+		time.Sleep(time.Duration(streamRate*mergeRate) * timeUnit)
+		go func() {
+			client.MakeRequest[pb.BadArray](sketch, SERVER_ADR, c.BadCount, conn, &c, startRealConnection, reconAttempt)
 			atomic.AddUint64(&meregesMade, 1)
 		}()
 	}
@@ -80,20 +155,50 @@ func main() {
 	var mu sync.Mutex
 	cond := sync.NewCond(&mu)
 	dataStream := *stream.NewStreamFromCsv[float64](DATA_SET_PATH, HEADER_NAME, streamRate, -1)
-	sketch := client.GetKll(100, mergeRate, dataStream)
+	kllSketch := client.GetKll(100, mergeRate, dataStream)
+	countSketch := client.GetCount(mergeRate, dataStream)
+	badArr := client.GetBad(mergeRate, dataStream)
+	var sketchSize uint64
 
 	for range clientAmmount {
 		wg.Add(1)
 
-		go startKll[float64](&wg, &fg, cond, streamRate, mergeRate, sketch, NUM_MERGES)
+		if sketchType == "kll" {
+			go startKll[float64](&wg, &fg, cond, streamRate, mergeRate, kllSketch, NUM_MERGES)
+		}
+		if sketchType == "count" {
+			go startCount[float64](&wg, &fg, cond, streamRate, mergeRate, countSketch, NUM_MERGES)
+		}
+		if sketchType == "badCount" {
+			go startBadCount[float64](&wg, &fg, cond, streamRate, mergeRate, badArr, NUM_MERGES)
+		}
+		if sketchType == "badKll" {
+			go startBadKll[float64](&wg, &fg, cond, streamRate, mergeRate, badArr, NUM_MERGES)
+		}
+
+	}
+	if sketchType == "kll" {
+		sketchSize = uint64(proto.Size(kllSketch))
+	}
+	if sketchType == "count" {
+		sketchSize = uint64(proto.Size(kllSketch))
+	}
+	if sketchType == "badCount" {
+		sketchSize = uint64(proto.Size(kllSketch))
+	}
+	if sketchType == "badKll" {
+		sketchSize = uint64(proto.Size(kllSketch))
 	}
 	wg.Wait()
 
 	cond.Broadcast()
-	optimal := 5.0 / (float64(streamRate) * float64(mergeRate)) * float64(clientAmmount) * 1e9
+	optimal := math.Round(mesuringInterval / (float64(streamRate) * float64(mergeRate)) * float64(clientAmmount) * timeExponent)
+	optBandWidth := int64(math.Min(1e9/3.0, float64(sketchSize)*optimal/mesuringInterval))
+
 	for {
-		time.Sleep(time.Duration(5) * time.Second)
-		fmt.Println(atomic.SwapUint64(&meregesMade, 0), optimal)
+		time.Sleep(time.Duration(mesuringInterval) * time.Second)
+		merges := atomic.SwapUint64(&meregesMade, 0)
+		fmt.Println(merges, optimal, sketchSize*merges/uint64(mesuringInterval), optBandWidth)
 	}
 
 }
